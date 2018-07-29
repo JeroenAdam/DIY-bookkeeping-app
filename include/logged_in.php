3adb3c5479bdd0cce06d80a3e36f08cf503487e0
JeroenAdam <info@adambahri.com>
2018-07-29 11:21:59 +0000

    Delete logged_in.php

--- include/logged_in.php
+++
@@ -1,7 +0,0 @@
-<?php session_start();
-if (!isset($_SESSION["username"])){
-	$tekst = "<h2>U bent nog niet aangemeld.</h2>
-		U kunt <a href=\"index.php\">hier inloggen</a>";
-	echo($tekst);
-	exit();
-}?>
